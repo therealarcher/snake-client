1776f0077c74581b39c83aff6ef79a07530fb03c
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-03 20:04:50 +0000

    added client.js which exports its connection to play.js

---
+++ client.js
@@ -0,0 +1,26 @@
+/**
+ * Establishes connection with the game server
+ */
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.0.110',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+  conn.on('connect', () => {
+    conn.write('Name: JDA');
+  });
+
+  return conn;
+};
+
+module.exports = {connect};
