65844722fe8913a4c93093dca7f705f4f8f09b3a
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-03 22:03:58 +0000

    added user input functionality for ctrl C-exit

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
 const {connect} = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+    };
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+}
+
+
+setupInput();
+
+
+
